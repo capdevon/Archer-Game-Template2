eb46289a5860c7513a5c88c446407273ce04ff33
Wyatt Gillette <jefferydeaver2010@gmail.com>
2020-12-07 21:34:47 +0100

    Update AnimUtils3.java

--- src/main/java/com/capdevon/anim/AnimUtils3.java
+++ src/main/java/com/capdevon/anim/AnimUtils3.java
@@ -28,10 +28,10 @@ import com.jme3.util.SafeArrayList;
  */
 public class AnimUtils3 {
 
-    /**
-     * @param from
-     * @param to 
-     */
+	/**
+	 * @param from
+	 * @param to
+	 */
 	public static void copyAnimation(Spatial from, Spatial to) {
 
 		AnimComposer source = getAnimControl(from);
@@ -40,7 +40,7 @@ public class AnimUtils3 {
 
 		copyAnimation(source, target, targetArmature);
 	}
-	
+
 	/**
 	 * 
 	 * @param source
@@ -53,12 +53,12 @@ public class AnimUtils3 {
 				System.out.println("Copying Animation: " + animName);
 
 				AnimClip clip = new AnimClip(animName);
-				clip.setTracks( copyAnimTracks(source.getAnimClip(animName), targetArmature) );
+				clip.setTracks(copyAnimTracks(source.getAnimClip(animName), targetArmature));
 				target.addAnimClip(clip);
 			}
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param sourceClip
@@ -66,13 +66,13 @@ public class AnimUtils3 {
 	 * @return
 	 */
 	private static AnimTrack[] copyAnimTracks(AnimClip sourceClip, Armature targetArmature) {
-		
+
 		SafeArrayList<AnimTrack> tracks = new SafeArrayList<>(AnimTrack.class);
-		
+
 		for (AnimTrack track : sourceClip.getTracks()) {
 
 			TransformTrack tt = (TransformTrack) track;
-			
+
 			if (tt.getTarget() instanceof Joint) {
 				Joint joint = (Joint) tt.getTarget();
 				HasLocalTransform target = targetArmature.getJoint(joint.getName());
@@ -85,32 +85,32 @@ public class AnimUtils3 {
 
 		return tracks.getArray();
 	}
-    
-    public static AnimComposer getAnimControl(Spatial sp) {
-    	AnimComposer control = findControl(sp, AnimComposer.class);
-        if (control == null) {
-            throw new IllegalArgumentException("AnimComposer not found: " + sp);
-        }
-        return control;
-    }
-    
-    public static SkinningControl getSkeletonControl(Spatial sp) {
-    	SkinningControl control = findControl(sp, SkinningControl.class);
-        if (control == null) {
-            throw new IllegalArgumentException("SkinningControl not found: " + sp);
-        }
-        return control;
-    }
-    
-    public static Joint findBone(Spatial sp, String boneName) {
-    	SkinningControl skControl = getSkeletonControl(sp);
-    	Joint bone = skControl.getArmature().getJoint(boneName);
-    	if (bone == null) {
-    		throw new IllegalArgumentException("Armature Joint not found: " + boneName);
-    	}
-    	return bone;
-    }
-    
+
+	public static AnimComposer getAnimControl(Spatial sp) {
+		AnimComposer control = findControl(sp, AnimComposer.class);
+		if (control == null) {
+			throw new IllegalArgumentException("AnimComposer not found: " + sp);
+		}
+		return control;
+	}
+
+	public static SkinningControl getSkeletonControl(Spatial sp) {
+		SkinningControl control = findControl(sp, SkinningControl.class);
+		if (control == null) {
+			throw new IllegalArgumentException("SkinningControl not found: " + sp);
+		}
+		return control;
+	}
+
+	public static Joint findBone(Spatial sp, String boneName) {
+		SkinningControl skControl = getSkeletonControl(sp);
+		Joint bone = skControl.getArmature().getJoint(boneName);
+		if (bone == null) {
+			throw new IllegalArgumentException("Armature Joint not found: " + boneName);
+		}
+		return bone;
+	}
+
 	public static Node getAttachments(Spatial sp, String boneName) {
 		SkinningControl skControl = getSkeletonControl(sp);
 		Node attachedNode = skControl.getAttachmentsNode(boneName);
@@ -119,14 +119,14 @@ public class AnimUtils3 {
 		}
 		return attachedNode;
 	}
-	
+
 	public static List<String> listBones(Spatial sp) {
 		SkinningControl skControl = getSkeletonControl(sp);
 		List<String> lst = listBones(skControl.getArmature());
 		Collections.sort(lst);
 		return lst;
 	}
-	
+
 	public static List<String> listBones(Armature skeleton) {
 		int boneCount = skeleton.getJointCount();
 		List<String> lst = new ArrayList<>(boneCount);
@@ -138,26 +138,26 @@ public class AnimUtils3 {
 		return lst;
 	}
 
-    /**
-     * @param <T>
-     * @param sp
-     * @param clazz
-     * @return 
-     */
-    private static <T extends Control> T findControl(Spatial sp, Class<T> clazz) {
-        T control = sp.getControl(clazz);
-        if (control != null) {
-            return control;
-        }
-        if (sp instanceof Node) {
-            for (Spatial child : ((Node) sp).getChildren()) {
-                control = findControl(child, clazz);
-                if (control != null) {
-                    return control;
-                }
-            }
-        }
-        return null;
-    }
+	/**
+	 * @param <T>
+	 * @param sp
+	 * @param clazz
+	 * @return
+	 */
+	private static <T extends Control> T findControl(Spatial sp, Class<T> clazz) {
+		T control = sp.getControl(clazz);
+		if (control != null) {
+			return control;
+		}
+		if (sp instanceof Node) {
+			for (Spatial child : ((Node) sp).getChildren()) {
+				control = findControl(child, clazz);
+				if (control != null) {
+					return control;
+				}
+			}
+		}
+		return null;
+	}
 
 }
