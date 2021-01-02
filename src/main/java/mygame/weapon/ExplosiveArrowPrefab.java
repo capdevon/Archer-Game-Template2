94016e291feb3793b15bdbec689493e716ef209b
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-01-02 13:22:20 +0100

    Update ExplosiveArrowPrefab.java

--- src/main/java/mygame/weapon/ExplosiveArrowPrefab.java
+++ src/main/java/mygame/weapon/ExplosiveArrowPrefab.java
@@ -3,6 +3,7 @@ package mygame.weapon;
 import com.jme3.app.Application;
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.PhysicsCollisionObject;
+import com.jme3.bullet.collision.shapes.SphereCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
@@ -15,51 +16,52 @@ import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Sphere;
 
 public class ExplosiveArrowPrefab extends RangedBullet {
-	
-	ExplosionPrefab explosionPrefab;
-	
-	public ExplosiveArrowPrefab(Application app, String name, String effectName, ColorRGBA effectColor, float effectDuration) {
-		super(app);
-		this.name = name;
-		mass = 6f;
-		explosionPrefab = new ExplosionPrefab(app);
-		explosionPrefab.assetName = effectName;
-		explosionPrefab.explosionColor = effectColor;
-		explosionPrefab.lifeTimeVFX = effectDuration;
-	}
 
-	@Override
-	public Spatial getAssetModel() {
-		// TODO Auto-generated method stub
-		Mesh mesh = new Sphere(16, 16, 0.05f);
-		Geometry geo = new Geometry("Arrow", mesh);
-		Material mat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-		mat.setColor("Color", ColorRGBA.Green.clone());
-		geo.setMaterial(mat);
-		return geo;
-	}
-	
-	@Override
-	public Spatial instantiate(Vector3f position, Quaternion rotation, Node parent) {
-    	Spatial model = getAssetModel();
-    	model.setName(name + "-" + nextSeqId());
-    	model.setLocalTranslation(position);
-    	model.setLocalRotation(rotation);
-    	parent.attachChild(model);
-    	
-    	// Add Physics.
-        RigidBodyControl rgb = new RigidBodyControl(mass);
+    private float radius = 0.04f;
+    private ExplosionPrefab explosionPrefab;
+
+    public ExplosiveArrowPrefab(Application app, String name, float mass, ExplosionPrefab explosionPrefab) {
+        super(app);
+        this.name = name;
+        this.mass = mass;
+        this.explosionPrefab = explosionPrefab;
+    }
+
+    @Override
+    public Spatial loadModel() {
+        //		Mesh mesh = new Sphere(16, 16, 0.05f);
+        //		Geometry geo = new Geometry("Arrow", mesh);
+        //		Material mat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        //		mat.setColor("Color", ColorRGBA.Green.clone());
+        //		geo.setMaterial(mat);
+        //		return geo;
+
+        return app.getAssetManager().loadModel("Models/Arrow/arrow.glb");
+    }
+
+    @Override
+    public Spatial instantiate(Vector3f position, Quaternion rotation, Node parent) {
+        Spatial model = loadModel();
+        model.setName(name + "-" + nextSeqId());
+        model.setLocalTranslation(position);
+        model.setLocalRotation(rotation);
+        parent.attachChild(model);
+
+        // Add Physics
+        SphereCollisionShape shape = new SphereCollisionShape(radius);
+        RigidBodyControl rgb = new RigidBodyControl(shape, mass);
         model.addControl(rgb);
         PhysicsSpace.getPhysicsSpace().add(rgb);
+
         rgb.setCollisionGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
         rgb.setCollideWithGroups(PhysicsCollisionObject.COLLISION_GROUP_01);
         rgb.setCcdMotionThreshold(0.001f);
-        
+
         ExplosiveArrowControl arrow = new ExplosiveArrowControl();
         arrow.explosionPrefab = explosionPrefab;
         model.addControl(arrow);
-    	
-    	return model;
+
+        return model;
     }
 
 }
