c859c9f407ed5cbc0d06f8d50ada6eea600c737e
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-01-02 13:14:21 +0100

    Update ExplosionPrefab.java

--- src/main/java/mygame/weapon/ExplosionPrefab.java
+++ src/main/java/mygame/weapon/ExplosionPrefab.java
@@ -2,6 +2,7 @@ package mygame.weapon;
 
 import com.capdevon.control.TimerControl;
 import com.jme3.app.Application;
+import com.jme3.light.Light;
 import com.jme3.light.PointLight;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
@@ -10,55 +11,62 @@ import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 
 public class ExplosionPrefab extends PrefabComponent {
-	
-	String assetName;
-	ColorRGBA explosionColor;
-	float lifeTimeVFX = 5f;
 
-	private final PointLight lightVfx;
+    private String assetName;
+    private ColorRGBA explosionColor;
+    private float lifeTimeVFX = 5f;
 
-    public ExplosionPrefab(Application app) {
-    	super(app);
-    	lightVfx = new PointLight();
+    public ExplosionPrefab(Application app, String effectName, ColorRGBA effectColor, float effectDuration) {
+        super(app);
+        this.assetName = effectName;
+        this.explosionColor = effectColor;
+        this.lifeTimeVFX = effectDuration;
     }
 
-	@Override
-	public Spatial getAssetModel() {
-		return app.getAssetManager().loadModel(assetName);
-	}
+    @Override
+    public Spatial loadModel() {
+        return app.getAssetManager().loadModel(assetName);
+    }
 
-	@Override
-	public Spatial instantiate(Vector3f position, Quaternion rotation, Node parent) {
-		Node model = (Node) getAssetModel();
-		model.setName("Explosion-" + nextSeqId());
+    @Override
+    public Spatial instantiate(Vector3f position, Quaternion rotation, Node parent) {
+
+        Node model = (Node) loadModel();
+        model.setName("Explosion-" + nextSeqId());
         model.setLocalTranslation(position);
         parent.attachChild(model);
 
-        lightVfx.setPosition(position.add(0, 1f, 0));
-        lightVfx.setRadius(13);
-        lightVfx.setColor(explosionColor.mult(5));
-        app.getRootNode().addLight(lightVfx);
+        PointLight lightVFX = new PointLight();
+        lightVFX.setPosition(position.add(0, 1f, 0));
+        lightVFX.setRadius(13);
+        lightVFX.setColor(explosionColor.mult(5));
+        app.getRootNode().addLight(lightVFX);
 
         EmitterData emitter = new EmitterData(model);
         model.addControl(new TimerControl(lifeTimeVFX) {
-        	//Gonna abuse an existing control a bit to fade out the light
-	        @Override
-	        protected void controlUpdate(float tpf) {
-		        super.controlUpdate(tpf);
-		        lightVfx.setColor(explosionColor.mult(5).mult(1 - time/maxTime));
-	        }
 
-	        @Override
-			public void onTrigger() {
-				emitter.stop();
-	            spatial.removeFromParent();
-	            app.getRootNode().removeLight(lightVfx);
-	            System.out.println("Explosion removed: " + model);
-			}
+            @Override
+            protected void controlUpdate(float tpf) {
+                super.controlUpdate(tpf);
+                lightVFX.setColor(explosionColor.mult(5).mult(1 - time / maxTime));
+            }
+
+            @Override
+            public void onTrigger() {
+                emitter.stop();
+                spatial.removeFromParent();
+                app.getRootNode().removeLight(lightVFX);
+                System.out.println("Explosion removed: " + model);
+
+//	            System.out.println("debug Scene LightList:--------------------");
+//	            for (Light light : app.getRootNode().getLocalLightList()) {
+//	            	System.out.println(light);
+//	            }
+            }
         });
-        
+
         emitter.play();
-        
+
         return model;
-	}
+    }
 }
