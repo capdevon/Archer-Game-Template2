edb08f397069f6f764a55182bbf5cd35caa09237
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-01-02 12:53:34 +0100

    Update PhysxDebugAppState.java

--- src/main/java/com/capdevon/physx/PhysxDebugAppState.java
+++ src/main/java/com/capdevon/physx/PhysxDebugAppState.java
@@ -4,47 +4,52 @@ import com.jme3.app.Application;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.app.state.BaseAppState;
 import com.jme3.bullet.BulletAppState;
+import com.jme3.input.InputManager;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
 
+/**
+ * @author capdevon
+ */
 public class PhysxDebugAppState extends BaseAppState implements ActionListener {
-	
-	private final String TOGGLE_PHYSICS_DEBUG = "TOGGLE_PHYSICS_DEBUG";
-	
-	private AppStateManager stateManager;
-	private BulletAppState bulletAppState;
 
-	@Override
-	public void onAction(String name, boolean isPressed, float tpf) {
-		if (name.equals(TOGGLE_PHYSICS_DEBUG) && isPressed) {
+    private final String TOGGLE_PHYSICS_DEBUG = "TOGGLE_PHYSICS_DEBUG";
+
+    private InputManager inputManager;
+    private AppStateManager stateManager;
+    private BulletAppState bulletAppState;
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals(TOGGLE_PHYSICS_DEBUG) && isPressed) {
             boolean debug = bulletAppState.isDebugEnabled();
             bulletAppState.setDebugEnabled(!debug);
         }
-	}
-	
-	@Override
-	protected void initialize(Application app) {
-		this.stateManager = app.getStateManager();
-		this.bulletAppState = stateManager.getState(BulletAppState.class);
-		
-		app.getInputManager().addMapping(TOGGLE_PHYSICS_DEBUG, new KeyTrigger(KeyInput.KEY_0));
-		app.getInputManager().addListener(this, TOGGLE_PHYSICS_DEBUG);
-	}
-	
-	@Override
-	protected void cleanup(Application app) {
-		// TODO Auto-generated method stub
-	}
+    }
 
-	@Override
-	protected void onEnable() {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    protected void initialize(Application app) {
+        this.inputManager = app.getInputManager();
+        this.stateManager = app.getStateManager();
+        this.bulletAppState = stateManager.getState(BulletAppState.class);
 
-	@Override
-	protected void onDisable() {
-		// TODO Auto-generated method stub
-	}
+        inputManager.addListener(this, TOGGLE_PHYSICS_DEBUG);
+    }
+
+    @Override
+    protected void cleanup(Application app) {
+        inputManager.removeListener(this);
+    }
+
+    @Override
+    protected void onEnable() {
+        inputManager.addMapping(TOGGLE_PHYSICS_DEBUG, new KeyTrigger(KeyInput.KEY_0));
+    }
+
+    @Override
+    protected void onDisable() {
+        inputManager.deleteMapping(TOGGLE_PHYSICS_DEBUG);
+    }
 
 }
