f6ba544ea54c25a51bc8edaecc90e2fa27149f61
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-01-02 13:21:02 +0100

    Update Damageable.java

--- src/main/java/mygame/weapon/Damageable.java
+++ src/main/java/mygame/weapon/Damageable.java
@@ -1,51 +1,65 @@
 package mygame.weapon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.capdevon.control.AdapterControl;
 import com.capdevon.control.MaterializeTimer;
 import com.capdevon.control.MaterializerListener;
-import com.capdevon.engine.GameObject;
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.material.Material;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.SceneGraphVisitorAdapter;
+import com.jme3.scene.Spatial;
 
 public class Damageable extends AdapterControl {
-	
-	private boolean hasAppliedDamage;
-    
-	public void applyDamage() {
-		
-		if (hasAppliedDamage)
-			return;
-		
-		hasAppliedDamage = true;
-		List<Geometry> lstGeometries = GameObject.listGeometries(spatial);
-		
-		for (Geometry geo : lstGeometries) {
-			Material mat = geo.getMaterial();
 
-			if ("MaterializePBR".equals(mat.getName())) {
+    private boolean hasAppliedDamage;
 
-				MaterializeTimer mTimer = new MaterializeTimer(mat, false, 0.4f);
-				mTimer.setMaterializerListener(new MaterializerListener() {
-					@Override
-					public void onCompleteEvent() {
-						mTimer.setMaterializerListener(null);
-						geo.removeControl(mTimer);
-						destroy();
-					}
-				});
-				geo.addControl(mTimer);
-				System.out.println(spatial + "; MaterializeTimer added");
-			}
-		}
-	}
-	
-	private void destroy() {
-		PhysicsSpace.getPhysicsSpace().removeAll(spatial);
-		spatial.removeFromParent();
-		System.out.println("Destroyed: " + spatial);
-	}
+    public void applyDamage() {
+
+        if (hasAppliedDamage)
+            return;
+
+        hasAppliedDamage = true;
+        List <Geometry> lstGeometries = listGeometries(spatial);
+
+        for (Geometry geo: lstGeometries) {
+            Material mat = geo.getMaterial();
+
+            if ("MaterializePBR".equals(mat.getName())) {
+
+                MaterializeTimer mTimer = new MaterializeTimer(mat, false, 0.4f);
+                mTimer.setMaterializerListener(new MaterializerListener() {
+                    @Override
+                    public void onCompleteEvent() {
+                        mTimer.setMaterializerListener(null);
+                        geo.removeControl(mTimer);
+                        destroy();
+                    }
+                });
+                geo.addControl(mTimer);
+                System.out.println(spatial + "; MaterializeTimer added");
+            }
+        }
+    }
+
+    private List < Geometry > listGeometries(Spatial subtree) {
+        List < Geometry > storeResult = new ArrayList < > ();
+        subtree.breadthFirstTraversal(new SceneGraphVisitorAdapter() {
+            @Override
+            public void visit(Geometry geo) {
+                storeResult.add(geo);
+            }
+        });
+
+        return storeResult;
+    }
+
+    private void destroy() {
+        PhysicsSpace.getPhysicsSpace().removeAll(spatial);
+        spatial.removeFromParent();
+        System.out.println("Destroyed: " + spatial);
+    }
 
 }
