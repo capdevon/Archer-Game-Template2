2f3c51d4dd190cae978c6378a6801f79c01ad131
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-01-02 13:13:01 +0100

    Update PrefabComponent.java

--- src/main/java/mygame/weapon/PrefabComponent.java
+++ src/main/java/mygame/weapon/PrefabComponent.java
@@ -7,32 +7,40 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 
+/**
+ * @author capdevon
+ */
 public abstract class PrefabComponent {
-	
-	private int objectId = 0;
-	
-	SimpleApplication app;
-	
-	public PrefabComponent(Application app) {
-    	this.app = (SimpleApplication) app;
-    }
-	
-    public int nextSeqId() {
-    	return ++objectId;
-    }
-    
-    public abstract Spatial getAssetModel();
-    
-    public abstract Spatial instantiate(Vector3f position, Quaternion rotation, Node parent);
-    
+
+    private int objectId = 0;
+
+    SimpleApplication app;
+
     /**
-     * 
+     * @param app
+     */
+    public PrefabComponent(Application app) {
+        this.app = (SimpleApplication) app;
+    }
+
+    /**
+     * @return
+     */
+    public int nextSeqId() {
+        return ++objectId;
+    }
+
+    /**
      * @param position
      * @param rotation
      * @return
      */
     public Spatial instantiate(Vector3f position, Quaternion rotation) {
-    	return instantiate(position, rotation, app.getRootNode());
+        return instantiate(position, rotation, app.getRootNode());
     }
+
+    public abstract Spatial instantiate(Vector3f position, Quaternion rotation, Node parent);
+
+    public abstract Spatial loadModel();
 
 }
