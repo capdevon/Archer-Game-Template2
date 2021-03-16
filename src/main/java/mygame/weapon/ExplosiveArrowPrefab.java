b8b11da2bba791897e1d450c27e0a8c83bc46620
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-03-16 23:29:56 +0100

    Update ExplosiveArrowPrefab.java

--- src/main/java/mygame/weapon/ExplosiveArrowPrefab.java
+++ src/main/java/mygame/weapon/ExplosiveArrowPrefab.java
@@ -1,19 +1,13 @@
 package mygame.weapon;
 
 import com.jme3.app.Application;
-import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.PhysicsCollisionObject;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.Mesh;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Sphere;
 
 public class ExplosiveArrowPrefab extends RangedBullet {
 
@@ -31,12 +25,12 @@ public class ExplosiveArrowPrefab extends RangedBullet {
     public Spatial loadModel() {
         //		Mesh mesh = new Sphere(16, 16, 0.05f);
         //		Geometry geo = new Geometry("Arrow", mesh);
-        //		Material mat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        //		Material mat = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
         //		mat.setColor("Color", ColorRGBA.Green.clone());
         //		geo.setMaterial(mat);
         //		return geo;
 
-        return app.getAssetManager().loadModel("Models/Arrow/arrow.glb");
+        return getAssetManager().loadModel("Models/Arrow/arrow.glb");
     }
 
     @Override
@@ -51,7 +45,7 @@ public class ExplosiveArrowPrefab extends RangedBullet {
         SphereCollisionShape shape = new SphereCollisionShape(radius);
         RigidBodyControl rgb = new RigidBodyControl(shape, mass);
         model.addControl(rgb);
-        PhysicsSpace.getPhysicsSpace().add(rgb);
+        getPhysicsSpace().add(rgb);
 
         rgb.setCollisionGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
         rgb.setCollideWithGroups(PhysicsCollisionObject.COLLISION_GROUP_01);
