bd7024afcd5bf94b9320aa0e947f8c83d55883fe
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-03-16 23:26:36 +0100

    Update PrefabComponent.java

--- src/main/java/mygame/weapon/PrefabComponent.java
+++ src/main/java/mygame/weapon/PrefabComponent.java
@@ -2,45 +2,81 @@ package mygame.weapon;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.PhysicsSpace;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.system.AppSettings;
 
 /**
  * @author capdevon
  */
 public abstract class PrefabComponent {
 
-    private int objectId = 0;
+	private final Application app;
+	private final AssetManager assetManager;
+	private final AppStateManager stateManager;
+	private final AppSettings settings;
+	private int objectId = 0;
 
-    SimpleApplication app;
+	public PrefabComponent(Application app) {
+		this.app            = app;
+		this.assetManager   = app.getAssetManager();
+		this.stateManager   = app.getStateManager();
+		this.settings       = app.getContext().getSettings();
+	}
 
-    /**
-     * @param app
-     */
-    public PrefabComponent(Application app) {
-        this.app = (SimpleApplication) app;
-    }
+	public Application getApplication() {
+		return app;
+	}
 
-    /**
-     * @return
-     */
-    public int nextSeqId() {
-        return ++objectId;
-    }
+	public final AssetManager getAssetManager() {
+		return assetManager;
+	}
 
-    /**
-     * @param position
-     * @param rotation
-     * @return
-     */
-    public Spatial instantiate(Vector3f position, Quaternion rotation) {
-        return instantiate(position, rotation, app.getRootNode());
-    }
+	public final <T extends AppState> T getState(Class<T> type) {
+		return stateManager.getState(type);
+	}
 
-    public abstract Spatial instantiate(Vector3f position, Quaternion rotation, Node parent);
+	public final PhysicsSpace getPhysicsSpace() {
+		return stateManager.getState(BulletAppState.class).getPhysicsSpace();
+	}
 
-    public abstract Spatial loadModel();
+	public final AppSettings getSettings() {
+		return settings;
+	}
+
+	public Node getRootNode() {
+		return ((SimpleApplication) app).getRootNode();
+	}
+
+	public Node getGuiNode() {
+		return ((SimpleApplication) app).getGuiNode();
+	}
+
+	/**
+	 * @return
+	 */
+	public int nextSeqId() {
+		return ++objectId;
+	}
+
+	/**
+	 * @param position
+	 * @param rotation
+	 * @return
+	 */
+	public Spatial instantiate(Vector3f position, Quaternion rotation) {
+		return instantiate(position, rotation, getRootNode());
+	}
+
+	public abstract Spatial instantiate(Vector3f position, Quaternion rotation, Node parent);
+
+	public abstract Spatial loadModel();
 
 }
