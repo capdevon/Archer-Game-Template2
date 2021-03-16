1cda6bf221495a572972911a2b4a90d3a9f2e02a
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-03-16 23:28:20 +0100

    Update ArrowPrefab.java

--- src/main/java/mygame/weapon/ArrowPrefab.java
+++ src/main/java/mygame/weapon/ArrowPrefab.java
@@ -1,20 +1,13 @@
 package mygame.weapon;
 
-import com.capdevon.engine.FVector;
 import com.jme3.app.Application;
-import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.PhysicsCollisionObject;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
 import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.debug.Arrow;
-import com.jme3.scene.shape.Sphere;
 
 /**
  * @author capdevon
@@ -34,13 +27,13 @@ public class ArrowPrefab extends RangedBullet {
 //		Node model = new Node();
 //
 //		Geometry g1 = new Geometry("Arrow.GeoMesh", new Sphere(16, 16, radius));
-//		Material mat1 = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+//		Material mat1 = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
 //		mat1.setColor("Color", ColorRGBA.Green.clone());
 //		g1.setMaterial(mat1);
 //		model.attachChild(g1);
 //
 //		Geometry g2 = new Geometry("Axis.Z", new Arrow(Vector3f.UNIT_Z));
-//		Material mat2 = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+//		Material mat2 = new Material(getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
 //		mat2.setColor("Color", ColorRGBA.Blue.clone());
 //		mat2.getAdditionalRenderState().setLineWidth(2f);
 //		g2.setMaterial(mat2);
@@ -49,7 +42,7 @@ public class ArrowPrefab extends RangedBullet {
 //
 //		return model;
 
-		return app.getAssetManager().loadModel("Models/Arrow/arrow.glb");
+		return getAssetManager().loadModel("Models/Arrow/arrow.glb");
 	}
 
     @Override
@@ -64,7 +57,7 @@ public class ArrowPrefab extends RangedBullet {
         SphereCollisionShape shape = new SphereCollisionShape(radius);
         RigidBodyControl rgb = new RigidBodyControl(shape, mass);
         model.addControl(rgb);
-        PhysicsSpace.getPhysicsSpace().add(rgb);
+        getPhysicsSpace().add(rgb);
 
         rgb.setCollisionGroup(PhysicsCollisionObject.COLLISION_GROUP_02);
         rgb.setCollideWithGroups(PhysicsCollisionObject.COLLISION_GROUP_01);
