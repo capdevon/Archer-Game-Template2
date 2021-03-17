7c2e9e6ab5c49b68c7b59b7ef8763723b4633401
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-03-17 17:19:43 +0100

    Update FireWeapon.java

--- src/main/java/mygame/weapon/FireWeapon.java
+++ src/main/java/mygame/weapon/FireWeapon.java
@@ -1,12 +1,13 @@
 package mygame.weapon;
 
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.capdevon.physx.Physics;
-import com.capdevon.physx.PhysxQuery;
 import com.capdevon.physx.RaycastHit;
 import com.jme3.audio.AudioNode;
+import com.jme3.bullet.collision.PhysicsCollisionObject;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.bullet.objects.PhysicsRigidBody;
 import com.jme3.effect.ParticleEmitter;
@@ -16,55 +17,81 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 
+/**
+ * @author capdevon
+ */
 public class FireWeapon extends Weapon {
-	
-	private static final Logger logger = Logger.getLogger(FireWeapon.class.getName());
-	
-	float distance = 30f;
-	AudioNode impactSFX;
-	ParticleEmitter impactVFX;
-	RaycastHit shootHit = new RaycastHit();
 
-	public FireWeapon() {
-		// default empty.
-	}
-	
-	/**
-	 * @param name
-	 * @param model
-	 */
-	public FireWeapon(String name, Node model) {
-		super(name, model);
-	}
-	
-	public void handleShoot(Vector3f origin, Vector3f direction) {
-		if (Physics.doRaycast(origin, direction, shootHit, distance)) {
-			logger.log(Level.INFO, " * You shot: " + shootHit);
-			applyExplosion(shootHit);
+    private static final Logger logger = Logger.getLogger(FireWeapon.class.getName());
 
-		} else {
-			logger.log(Level.INFO, "Target not in range...");
-		}
-		
-		if (impactSFX != null) {
-			impactSFX.playInstance();
-		}
-	}
-	
+    float distance = 30f;
+    AudioNode impactSFX;
+    ParticleEmitter impactVFX;
+    RaycastHit shootHit = new RaycastHit();
+
+    public FireWeapon() {
+        // default empty.
+    }
+
+    /**
+     * @param name
+     * @param model
+     */
+    public FireWeapon(String name, Node model) {
+        super(name, model);
+    }
+
+    public void handleShoot(Vector3f origin, Vector3f direction) {
+        if (Physics.doRaycast(origin, direction, shootHit, distance)) {
+            logger.log(Level.INFO, " * You shot: " + shootHit);
+            applyExplosion(shootHit);
+
+        } else {
+            logger.log(Level.INFO, "Target not in range...");
+        }
+
+        if (impactSFX != null) {
+            impactSFX.playInstance();
+        }
+    }
+
     /**
      * @param hit
      */
     private void applyExplosion(RaycastHit hit) {
         float explosionRadius = 5;
         float baseStrength = 10f;
+        int layerMask = PhysicsCollisionObject.COLLISION_GROUP_01;
         ColorRGBA color = ColorRGBA.randomColor();
 
-        for (PhysicsRigidBody rb : PhysxQuery.overlapSphere(hit.point, explosionRadius, x -> x.getMass() > 0)) {
-        	Physics.addExplosionForce(rb, baseStrength, hit.point, explosionRadius);
-            Spatial userObj = (Spatial) rb.getUserObject();
-            applyDamage(userObj, color);
+        Function<PhysicsCollisionObject, Boolean> dynamicBodies = new Function<PhysicsCollisionObject, Boolean>() {
+            @Override
+            public Boolean apply(PhysicsCollisionObject pco) {
+                if (pco instanceof PhysicsRigidBody) {
+                    PhysicsRigidBody rb = (PhysicsRigidBody) pco;
+                    return rb.getMass() > 0;
+                }
+                return false;
+            }
+        };
+
+        int maxColliders = 10;
+        PhysicsCollisionObject[] hitColliders = new PhysicsCollisionObject[maxColliders];
+        int numColliders = Physics.overlapSphereNonAlloc(hit.point, explosionRadius, hitColliders, layerMask, dynamicBodies);
+        System.out.println("numColliders=" + numColliders);
+
+        for (int i = 0; i < numColliders; i++) {
+
+            PhysicsCollisionObject pco = hitColliders[i];
+            if (pco instanceof PhysicsRigidBody) {
+
+                PhysicsRigidBody rb = (PhysicsRigidBody) pco;
+                Physics.addExplosionForce(rb, baseStrength, hit.point, explosionRadius);
+
+                Spatial userObj = (Spatial) pco.getUserObject();
+                applyDamage(userObj, color);
+            }
         }
-//        particleManager.playEffect(weapon.getEffectName(), shootHit.point, 10f);
     }
 
     /**
@@ -80,7 +107,6 @@ public class FireWeapon extends Weapon {
             ColorRGBA color = ColorRGBA.randomColor();
             applyDamage(hit.userObject, color);
         }
-//        particleManager.playEffect(weapon.getEffectName(), shootHit.point, 10f);
     }
 
     private void applyDamage(Spatial sp, ColorRGBA color) {
