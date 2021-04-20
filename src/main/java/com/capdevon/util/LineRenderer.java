a04063f8ba9bb82be3722e7e99b448fd6a191b58
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-20 10:34:55 +0200

    format code

--- src/main/java/com/capdevon/util/LineRenderer.java
+++ src/main/java/com/capdevon/util/LineRenderer.java
@@ -2,76 +2,115 @@ package com.capdevon.util;
 
 import java.util.List;
 
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
+import com.jme3.material.RenderState.FaceCullMode;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Spline;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial.CullHint;
+import com.jme3.scene.control.AbstractControl;
 import com.jme3.scene.shape.Curve;
 
-public class LineRenderer {
+/**
+ * 
+ * @author capdevon
+ */
+public class LineRenderer extends AbstractControl {
 
-	private int nbSubSegments = 1;
-	private Spline spline;
-	private Geometry geom;
-	private Material material;
-	
-	public LineRenderer(AssetManager assetManager, String shapeName) {
-		this(assetManager, 1, ColorRGBA.Green, shapeName);
-	}
-	
-	public LineRenderer(AssetManager assetManager, float lineWidth, ColorRGBA color, String shapeName) {
-		spline = new Spline();
-		spline.addControlPoint(new Vector3f(0, -1000f, 0));
-		geom = new Geometry(shapeName, new Curve(spline, nbSubSegments));
-		material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        material.setColor("Color", color.clone());
-        material.getAdditionalRenderState().setLineWidth(lineWidth);
-        geom.setMaterial(material);
-	}
-	
-	public void clearPoints() {
-		spline.clearControlPoints();
-	}
-	
-	public void addPoint(Vector3f point) {
-		spline.addControlPoint(point);
-	}
-	
-	public void setPoints(List<Vector3f> points) {
-		spline.clearControlPoints();
-		for (Vector3f point : points) {
-			spline.addControlPoint(point);
-		}
-	}
-	
-	public void updateGeometry() {
-		geom.setMesh(new Curve(spline, 1));
-		geom.updateModelBound();
-	}
-	
-	public boolean isEnabled() {
-        boolean isEnabled = (geom.getCullHint() == CullHint.Never || geom.getCullHint() == CullHint.Dynamic);
-        return isEnabled;
+    private int nbSubSegments = 1;
+    private Spline spline;
+    private Geometry geom;
+    private Material mat;
+    private boolean refresh;
+
+    /**
+     * Constructor
+     */
+    public LineRenderer(Application app) {
+        Node rootNode = ((SimpleApplication) app).getRootNode();
+        AssetManager assetManager = app.getAssetManager();
+        init(rootNode, assetManager);
+    }
+
+    /**
+     * @param rootNode
+     * @param assetManager
+     */
+    private void init(Node rootNode, AssetManager assetManager) {
+
+        mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.White);
+        mat.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Off);
+
+        spline = new Spline();
+        spline.addControlPoint(new Vector3f(0, -1000f, 0));
+        geom = new Geometry("LineRenderer", new Curve(spline, nbSubSegments));
+        geom.setMaterial(mat);
+        geom.setShadowMode(ShadowMode.Off);
+        rootNode.attachChild(geom);
+    }
+
+    public void clearPoints() {
+        spline.clearControlPoints();
+    }
+
+    public void addPoint(Vector3f point) {
+        spline.addControlPoint(point);
+    }
+
+    public void setPoints(List<Vector3f> points) {
+        spline.clearControlPoints();
+        for (Vector3f point: points) {
+            spline.addControlPoint(point);
+        }
+    }
+
+    public void updateGeometry() {
+        geom.setMesh(new Curve(spline, 1));
+        geom.updateModelBound();
+    }
+
+    public boolean isEnabled() {
+        return (geom.getCullHint() == CullHint.Never || geom.getCullHint() == CullHint.Dynamic);
     }
 
     public void setEnabled(boolean enable) {
-    	geom.setCullHint(enable ? CullHint.Never : CullHint.Always);
+        geom.setCullHint(enable ? CullHint.Never : CullHint.Always);
     }
-    
+
     public void setColor(ColorRGBA color) {
-    	material.setColor("Color", color.clone());
+        if (color == null) {
+            throw new IllegalArgumentException("color is null");
+        }
+        mat.setColor("Color", color.clone());
     }
-    
+
     public void setLineWidth(float lineWidth) {
-    	material.getAdditionalRenderState().setLineWidth(lineWidth);
+        if (lineWidth <= 0) {
+            throw new IllegalArgumentException("lineWidth must be greater than 0");
+        }
+        mat.getAdditionalRenderState().setLineWidth(lineWidth);
     }
-    
-    public void setParent(Node parent) {
-    	parent.attachChild(geom);
+
+    @Override
+    protected void controlUpdate(float tpf) {
+        if (refresh) {
+            updateGeometry();
+            refresh = false;
+        }
     }
-	
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+        // TODO Auto-generated method stub
+    }
+
 }
