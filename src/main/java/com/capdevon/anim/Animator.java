5fa22a96e78aa95c69aeefc1881c344a03522c59
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-20 10:39:23 +0200

    format code

--- src/main/java/com/capdevon/anim/Animator.java
+++ src/main/java/com/capdevon/anim/Animator.java
@@ -18,89 +18,90 @@ import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 
 /**
- *
+ * 
+ * @author capdevon
  */
 public class Animator extends AdapterControl {
 
-	private static final Logger LOGGER = Logger.getLogger(Animator.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Animator.class.getName());
 
-	private SkeletonControl skControl;
-	private AnimControl animControl;
-	private AnimChannel animChannel;
+    private SkeletonControl skControl;
+    private AnimControl animControl;
+    private AnimChannel animChannel;
 
-	@Override
-	public void setSpatial(Spatial sp) {
-		super.setSpatial(sp);
-		if (spatial != null) {
-			this.skControl = getComponentInChild(SkeletonControl.class);
-			this.animControl = getComponentInChild(AnimControl.class);
-			this.animChannel = animControl.createChannel();
-			System.out.println(spatial.getName() + " --Animations: " + animControl.getAnimationNames());
-		}
-	}
+    @Override
+    public void setSpatial(Spatial sp) {
+        super.setSpatial(sp);
+        if (spatial != null) {
+            this.skControl = getComponentInChild(SkeletonControl.class);
+            this.animControl = getComponentInChild(AnimControl.class);
+            this.animChannel = animControl.createChannel();
+            System.out.println(spatial.getName() + " --Animations: " + animControl.getAnimationNames());
+        }
+    }
 
-	public void setAnimation(String animName, LoopMode loopMode) {
-		if (hasAnimation(animName)) {
-			if (!animName.equals(animChannel.getAnimationName())) {
-				animChannel.setAnim(animName, .15f);
-				animChannel.setSpeed(1);
-				animChannel.setLoopMode(loopMode);
-			}
-		}
-	}
+    public void setAnimation(String animName, LoopMode loopMode) {
+        if (hasAnimation(animName)) {
+            if (!animName.equals(animChannel.getAnimationName())) {
+                animChannel.setAnim(animName, .15f);
+                animChannel.setSpeed(1);
+                animChannel.setLoopMode(loopMode);
+            }
+        }
+    }
 
-	public void setAnimation(Animation3 animation) {
-		if (hasAnimation(animation.name)) {
-			if (!animation.name.equals(animChannel.getAnimationName())) {
-				animChannel.setAnim(animation.name, animation.blendTime);
-				animChannel.setSpeed(animation.speed);
-				animChannel.setLoopMode(animation.loopMode);
-			}
-		}
-	}
+    public void setAnimation(Animation3 animation) {
+        if (hasAnimation(animation.name)) {
+            if (!animation.name.equals(animChannel.getAnimationName())) {
+                animChannel.setAnim(animation.name, animation.blendTime);
+                animChannel.setSpeed(animation.speed);
+                animChannel.setLoopMode(animation.loopMode);
+            }
+        }
+    }
 
-	public void crossFade(Animation3 newAnim) {
-		float dt = animChannel.getTime();
-		animChannel.setAnim(newAnim.name, newAnim.blendTime);
-		animChannel.setSpeed(newAnim.speed);
-		animChannel.setLoopMode(newAnim.loopMode);
-		animChannel.setTime(dt);
-	}
+    public void crossFade(Animation3 newAnim) {
+        float dt = animChannel.getTime();
+        animChannel.setAnim(newAnim.name, newAnim.blendTime);
+        animChannel.setSpeed(newAnim.speed);
+        animChannel.setLoopMode(newAnim.loopMode);
+        animChannel.setTime(dt);
+    }
 
-	private boolean hasAnimation(String animName) {
-		boolean result = animControl.getAnimationNames().contains(animName);
-		if (!result) {
-			LOGGER.log(Level.WARNING, "Cannot find animation named: {0}", animName);
-		}
-		return result;
-	}
+    private boolean hasAnimation(String animName) {
+        boolean result = animControl.getAnimationNames().contains(animName);
+        if (!result) {
+            LOGGER.log(Level.WARNING, "Cannot find animation named: {0}", animName);
+        }
+        return result;
+    }
 
-	public Bone getBone(String boneName) {
-		return skControl.getSkeleton().getBone(boneName);
-	}
+    public Bone getBone(String boneName) {
+        return skControl.getSkeleton().getBone(boneName);
+    }
 
-	public Node getAttachments(String boneName) {
-		return skControl.getAttachmentsNode(boneName);
-	}
+    public Node getAttachments(String boneName) {
+        return skControl.getAttachmentsNode(boneName);
+    }
 
-	public Spatial getRootMotion() {
-		return animControl.getSpatial();
-	}
+    public Spatial getRootMotion() {
+        return animControl.getSpatial();
+    }
 
-	public String getAnimationName() {
-		return animChannel.getAnimationName();
-	}
+    public String getAnimationName() {
+        return animChannel.getAnimationName();
+    }
 
-	public float getDeltaTime() {
-		return animChannel.getTime() / animChannel.getAnimMaxTime();
-	}
+    public float getDeltaTime() {
+        return animChannel.getTime() / animChannel.getAnimMaxTime();
+    }
 
-	public void addAnimListener(AnimEventListener listener) {
-		animControl.addListener(listener);
-	}
+    public void addAnimListener(AnimEventListener listener) {
+        animControl.addListener(listener);
+    }
 
-	public void removeAnimListener(AnimEventListener listener) {
-		animControl.removeListener(listener);
-	}
+    public void removeAnimListener(AnimEventListener listener) {
+        animControl.removeListener(listener);
+    }
 
 }
