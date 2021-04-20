942e9d48d3c86d233b443543f89fb1b7f7898cbf
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-20 10:42:16 +0200

    format code

--- src/main/java/com/capdevon/engine/Capture.java
+++ src/main/java/com/capdevon/engine/Capture.java
@@ -11,26 +11,26 @@ import java.io.File;
  */
 public class Capture {
 
-	/**
-	 * @param app
-	 * @param quality [0.5f]
-	 * @param dirName
-	 */
-	public static void captureVideo(Application app, float quality, String dirName) {
-		AppSettings settings = app.getContext().getSettings();
-		long fileId = (System.currentTimeMillis() / 1000);
-		String fileName = settings.getTitle() + "-" + fileId + ".avi";
-		File file = new File(dirName, fileName);
+    /**
+     * @param app
+     * @param quality [0.5f]
+     * @param dirName
+     */
+    public static void captureVideo(Application app, float quality, String dirName) {
+        AppSettings settings = app.getContext().getSettings();
+        long fileId = (System.currentTimeMillis() / 1000);
+        String fileName = settings.getTitle() + "-" + fileId + ".avi";
+        File file = new File(dirName, fileName);
 
-		int frameRate = settings.getFrameRate();
-		if (settings.getFrameRate() < 0) {
-			throw new IllegalArgumentException("FrameRate must not be negative: " + frameRate);
-		}
+        int frameRate = settings.getFrameRate();
+        if (settings.getFrameRate() < 0) {
+            throw new IllegalArgumentException("FrameRate must not be negative: " + frameRate);
+        }
 
-		VideoRecorderAppState recorder = new VideoRecorderAppState(file, quality, frameRate);
-		app.getStateManager().attach(recorder);
+        VideoRecorderAppState recorder = new VideoRecorderAppState(file, quality, frameRate);
+        app.getStateManager().attach(recorder);
 
-		System.out.println("Start VideoRecorder=" + file.getAbsolutePath());
-	}
+        System.out.println("Start VideoRecorder=" + file.getAbsolutePath());
+    }
 
 }
