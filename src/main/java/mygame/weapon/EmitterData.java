30db0c1413d6ab2ea0f8f8b7bd0cc4110d9d53e6
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-30 11:23:50 +0200

    format code

--- src/main/java/mygame/weapon/EmitterData.java
+++ src/main/java/mygame/weapon/EmitterData.java
@@ -5,34 +5,38 @@ import com.jme3.effect.ParticleEmitter;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 
+/**
+ * 
+ * @author capdevon
+ */
 public class EmitterData {
 
-	public Node emitter;
+    public Node emitter;
 
-	public EmitterData(Node emitter) {
-		this.emitter = emitter;
-	}
+    public EmitterData(Node emitter) {
+        this.emitter = emitter;
+    }
 
-	protected void stop() {
-		for (Spatial sp : emitter.getChildren()) {
-			if (sp instanceof AudioNode) {
-				((AudioNode) sp).stop();
+    protected void stop() {
+        for (Spatial sp: emitter.getChildren()) {
+            if (sp instanceof AudioNode) {
+                ((AudioNode) sp).stop();
 
-			} else if (sp instanceof ParticleEmitter) {
-				((ParticleEmitter) sp).killAllParticles();
-			}
-		}
-	}
+            } else if (sp instanceof ParticleEmitter) {
+                ((ParticleEmitter) sp).killAllParticles();
+            }
+        }
+    }
 
-	protected void play() {
-		for (Spatial sp : emitter.getChildren()) {
-			if (sp instanceof AudioNode) {
-				((AudioNode) sp).play();
+    protected void play() {
+        for (Spatial sp: emitter.getChildren()) {
+            if (sp instanceof AudioNode) {
+                ((AudioNode) sp).play();
 
-			} else if (sp instanceof ParticleEmitter) {
-				((ParticleEmitter) sp).emitAllParticles();
-			}
-		}
-	}
+            } else if (sp instanceof ParticleEmitter) {
+                ((ParticleEmitter) sp).emitAllParticles();
+            }
+        }
+    }
 
 }
