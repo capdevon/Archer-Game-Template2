879f358a228ee41dc5418264cdd5ebe40a1737fb
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-30 11:17:47 +0200

    added new methods

--- src/main/java/com/capdevon/engine/FRotator.java
+++ src/main/java/com/capdevon/engine/FRotator.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.capdevon.engine;
 
 import com.jme3.math.FastMath;
@@ -11,39 +6,75 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 
 /**
+ *
+ * @author capdevon
  */
 public class FRotator {
-    
+
     /**
-     * 
-     * @param direction
-     * @return 
+     * Creates a rotation which rotates from fromDirection to toDirection.
+     * (Default fromDirection = Vector3f.UNIT_Y)
+     *
+     * @param toDirection
+     * @return
      */
-    public static Quaternion fromToAngle(Vector3f direction) {
-        float angle = FastMath.atan2(direction.x, direction.z);
-        return new Quaternion().fromAngleAxis(angle, Vector3f.UNIT_Y);
+    public static Quaternion fromToRotation(Vector3f toDirection) {
+        return fromToRotation(toDirection, Vector3f.UNIT_Y);
     }
-    
+
     /**
-     * 
+     * Creates a rotation which rotates from fromDirection to toDirection.
+     *
      * @param direction
-     * @return 
+     * @param axis
+     * @return
      */
-    public static Quaternion lookAtRotation(Vector3f direction) {
+    public static Quaternion fromToRotation(Vector3f direction, Vector3f axis) {
+        float angle = FastMath.atan2(direction.x, direction.z);
+        return new Quaternion().fromAngleNormalAxis(angle, axis);
+    }
+
+    /**
+     * Creates a rotation with the specified forward and upwards directions.
+     * (Default upwards = Vector3f.UNIT_Y)
+     *
+     * @param direction
+     * @return
+     */
+    public static Quaternion lookRotation(Vector3f direction) {
         Quaternion q = new Quaternion();
         q.lookAt(direction, Vector3f.UNIT_Y);
         return q;
     }
 
     /**
-     * 
-     * @param sp
-     * @param direction
-     * @param changeAmnt 
+     * Spherically interpolates between quaternions a and b by ratio t. The
+     * parameter t is clamped to the range [0, 1].
+     *
+     * @param from
+     * @param to
+     * @param smoothTime
+     * @param viewDirection
+     * @return
      */
-    public static void slerp(Spatial sp, Vector3f direction, float changeAmnt) {
-        Quaternion q = fromToAngle(direction);
-        sp.getLocalRotation().slerp(q, changeAmnt);
+    public static Vector3f smoothDamp(Quaternion from, Quaternion to, float smoothTime, Vector3f viewDirection) {
+        if (viewDirection == null) {
+            viewDirection = new Vector3f();
+        }
+        from.slerp(to, FastMath.clamp(smoothTime, 0, 1));
+        return from.mult(Vector3f.UNIT_Z, viewDirection);
     }
-    
+
+    public static Vector3f eulerAngles(Spatial sp) {
+        float[] angles = new float[3];
+        sp.getWorldRotation().toAngles(angles);
+        return new Vector3f(angles[0], angles[1], angles[2]);
+    }
+
+    public static Vector3f localEulerAngles(Spatial sp) {
+        float[] angles = new float[3];
+        sp.getLocalRotation().toAngles(angles);
+        return new Vector3f(angles[0], angles[1], angles[2]);
+    }
+
 }
