074b72fcecdcafa502b848b6e3ac0b94b943d35b
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-30 10:43:05 +0200

    format code

--- src/main/java/mygame/WeaponUIManager.java
+++ src/main/java/mygame/WeaponUIManager.java
@@ -6,21 +6,25 @@ import com.jme3.scene.Spatial;
 
 import mygame.weapon.Weapon;
 
+/**
+ * 
+ * @author capdevon
+ */
 public class WeaponUIManager extends AdapterControl {
-	
-	public BitmapText weaponText;
-	PlayerControl m_PlayerControl;
-	
-	@Override
+
+    public BitmapText weaponText;
+    //PlayerControl m_PlayerControl;
+
+    @Override
     public void setSpatial(Spatial sp) {
         super.setSpatial(sp);
         if (spatial != null) {
-//        	m_PlayerControl = getComponent(PlayerControl.class);
+            //m_PlayerControl = getComponent(PlayerControl.class);
         }
-	}
-	
-	public void changeWeapon(Weapon weapon) {
-		weaponText.setText(weapon.getDescription());
-	}
+    }
+
+    public void changeWeapon(Weapon weapon) {
+        weaponText.setText(weapon.getDescription());
+    }
 
 }
