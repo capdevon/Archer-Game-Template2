d3f949035b3b5dfd4bcf5b129a1be9d0c6992dd8
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-09-30 11:22:09 +0200

    format code

--- src/main/java/mygame/weapon/PrefabComponent.java
+++ src/main/java/mygame/weapon/PrefabComponent.java
@@ -14,69 +14,70 @@ import com.jme3.scene.Spatial;
 import com.jme3.system.AppSettings;
 
 /**
+ * 
  * @author capdevon
  */
 public abstract class PrefabComponent {
 
-	private final Application app;
-	private final AssetManager assetManager;
-	private final AppStateManager stateManager;
-	private final AppSettings settings;
-	private int objectId = 0;
+    private final Application app;
+    private final AssetManager assetManager;
+    private final AppStateManager stateManager;
+    private final AppSettings settings;
+    private int objectId = 0;
 
-	public PrefabComponent(Application app) {
-		this.app            = app;
-		this.assetManager   = app.getAssetManager();
-		this.stateManager   = app.getStateManager();
-		this.settings       = app.getContext().getSettings();
-	}
+    public PrefabComponent(Application app) {
+        this.app = app;
+        this.assetManager = app.getAssetManager();
+        this.stateManager = app.getStateManager();
+        this.settings = app.getContext().getSettings();
+    }
 
-	public Application getApplication() {
-		return app;
-	}
+    public Application getApplication() {
+        return app;
+    }
 
-	public final AssetManager getAssetManager() {
-		return assetManager;
-	}
+    public final AssetManager getAssetManager() {
+        return assetManager;
+    }
 
-	public final <T extends AppState> T getState(Class<T> type) {
-		return stateManager.getState(type);
-	}
+    public final <T extends AppState> T getState(Class<T> type) {
+        return stateManager.getState(type);
+    }
 
-	public final PhysicsSpace getPhysicsSpace() {
-		return stateManager.getState(BulletAppState.class).getPhysicsSpace();
-	}
+    public final PhysicsSpace getPhysicsSpace() {
+        return stateManager.getState(BulletAppState.class).getPhysicsSpace();
+    }
 
-	public final AppSettings getSettings() {
-		return settings;
-	}
+    public final AppSettings getSettings() {
+        return settings;
+    }
 
-	public Node getRootNode() {
-		return ((SimpleApplication) app).getRootNode();
-	}
+    public Node getRootNode() {
+        return ((SimpleApplication) app).getRootNode();
+    }
 
-	public Node getGuiNode() {
-		return ((SimpleApplication) app).getGuiNode();
-	}
+    public Node getGuiNode() {
+        return ((SimpleApplication) app).getGuiNode();
+    }
 
-	/**
-	 * @return
-	 */
-	public int nextSeqId() {
-		return ++objectId;
-	}
+    /**
+     * @return
+     */
+    public int nextSeqId() {
+        return ++objectId;
+    }
 
-	/**
-	 * @param position
-	 * @param rotation
-	 * @return
-	 */
-	public Spatial instantiate(Vector3f position, Quaternion rotation) {
-		return instantiate(position, rotation, getRootNode());
-	}
+    /**
+     * @param position
+     * @param rotation
+     * @return
+     */
+    public Spatial instantiate(Vector3f position, Quaternion rotation) {
+        return instantiate(position, rotation, getRootNode());
+    }
 
-	public abstract Spatial instantiate(Vector3f position, Quaternion rotation, Node parent);
+    public abstract Spatial instantiate(Vector3f position, Quaternion rotation, Node parent);
 
-	public abstract Spatial loadModel();
+    public abstract Spatial loadModel();
 
 }
