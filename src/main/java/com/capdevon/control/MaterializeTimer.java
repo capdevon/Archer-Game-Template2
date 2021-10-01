83f62b7c84364431fa0ac4795e00bf41a74de854
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-10-01 13:18:11 +0200

    format code

--- src/main/java/com/capdevon/control/MaterializeTimer.java
+++ src/main/java/com/capdevon/control/MaterializeTimer.java
@@ -6,74 +6,78 @@ import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.control.AbstractControl;
 
+/**
+ * 
+ * @author capdevon
+ */
 public class MaterializeTimer extends AbstractControl {
 
-	private static final String EFFECT_TIME = "EffectTime";
-	private static final String MATERIALIZING = "Materializing";
+    private static final String EFFECT_TIME = "EffectTime";
+    private static final String MATERIALIZING = "Materializing";
 
-	private final Material material;
-	private final boolean fadeIn;
-	private final float speed;
-	private float time;
-	
-	private MaterializerListener listener;
+    private final Material material;
+    private final boolean fadeIn;
+    private final float speed;
+    private float time;
 
-	public MaterializeTimer(Material material, boolean fadeIn) {
-		this(material, fadeIn, 0.3f);
-	}
+    private MaterializerListener listener;
 
-	/**
-	 * Materialize a material.
-	 * 
-	 * @param material the material to materialize.
-	 * @param fadeIn   if true materialize in, else materialize out
-	 * @param speed    speed modifier between 0 and 1. 0.1 = 10 seconds, 1 = 1 second.
-	 */
-	public MaterializeTimer(Material material, boolean fadeIn, float speed) {
-		this.material = material;
-		this.material.setBoolean(MATERIALIZING, true);
-		this.material.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Off);
-		this.fadeIn = fadeIn;
-		this.speed = speed;
-		this.time = fadeIn ? 0.0f : 1.0f;
-	}
+    public MaterializeTimer(Material material, boolean fadeIn) {
+        this(material, fadeIn, 0.3f);
+    }
 
-	@Override
-	public void controlUpdate(float tpf) {
+    /**
+     * Materialize a material.
+     * 
+     * @param material the material to materialize.
+     * @param fadeIn   if true materialize in, else materialize out
+     * @param speed    speed modifier between 0 and 1. 0.1 = 10 seconds, 1 = 1 second.
+     */
+    public MaterializeTimer(Material material, boolean fadeIn, float speed) {
+        this.material = material;
+        this.material.setBoolean(MATERIALIZING, true);
+        this.material.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Off);
+        this.fadeIn = fadeIn;
+        this.speed = speed;
+        this.time = fadeIn ? 0f : 1f;
+    }
 
-		if (fadeIn) {
-			time += tpf * speed;
-		} else {
-			time -= tpf * speed;
-		}
+    @Override
+    public void controlUpdate(float tpf) {
 
-		material.setFloat(EFFECT_TIME, this.time);
+        if (fadeIn) {
+            time += tpf * speed;
+        } else {
+            time -= tpf * speed;
+        }
 
-		if ((fadeIn && time > 1.0) | (!fadeIn && time < 0)) {
-			material.setBoolean(MATERIALIZING, false);
-			material.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Back);
+        material.setFloat(EFFECT_TIME, this.time);
 
-			if (listener != null) {
-				listener.onCompleteEvent();
-			}
-		}
-	}
+        if ((fadeIn && time > 1.0) | (!fadeIn && time < 0)) {
+            material.setBoolean(MATERIALIZING, false);
+            material.getAdditionalRenderState().setFaceCullMode(RenderState.FaceCullMode.Back);
 
-	@Override
-	protected void controlRender(RenderManager rm, ViewPort vp) {
-		// TODO Auto-generated method stub
-	}
-	
-	public void setMaterializerListener(MaterializerListener listener) {
-		this.listener = listener;
-	}
+            if (listener != null) {
+                listener.onCompleteEvent();
+            }
+        }
+    }
 
-	public float getTime() {
-		return time;
-	}
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+        // TODO Auto-generated method stub
+    }
 
-	public Material getMaterial() {
-		return material;
-	}
+    public void setMaterializerListener(MaterializerListener listener) {
+        this.listener = listener;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
 
 }
