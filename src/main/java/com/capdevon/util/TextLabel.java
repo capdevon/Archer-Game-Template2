3b2937ba2904c1cc42b052e5b9843af02aeff1ac
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-10-01 14:15:17 +0200

    format code

--- src/main/java/com/capdevon/util/TextLabel.java
+++ src/main/java/com/capdevon/util/TextLabel.java
@@ -5,21 +5,20 @@ import com.jme3.scene.Node;
 
 public class TextLabel {
 
-	public Node guiNode;
-	public BitmapText bitmapText;
+    public final Node guiNode;
+    public final BitmapText bitmapText;
 
-	public TextLabel(Node guiNode, BitmapText bitmapText) {
-		super();
-		this.guiNode = guiNode;
-		this.bitmapText = bitmapText;
-	}
-	
-	public void setEnabled(boolean enabled) {
-		if (enabled) {
-			guiNode.attachChild(bitmapText);
-		} else {
-			guiNode.detachChild(bitmapText);
-		}
-	}
+    public TextLabel(Node guiNode, BitmapText bitmapText) {
+        this.guiNode = guiNode;
+        this.bitmapText = bitmapText;
+    }
+
+    public void setEnabled(boolean enabled) {
+        if (enabled) {
+            guiNode.attachChild(bitmapText);
+        } else {
+            guiNode.detachChild(bitmapText);
+        }
+    }
 
 }
