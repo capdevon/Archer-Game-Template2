17521b373599e8343e49ff2919b70a30b397d7e9
capdevon <jefferydeaver2010@gmail.com>
2022-11-06 17:16:06 +0100

    delete useless classes

--- src/main/java/mygame/Main.java
+++ src/main/java/mygame/Main.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mygame;
 
 import com.capdevon.audio.SoundManager;
-import com.capdevon.engine.Capture;
-import com.capdevon.engine.JMonkey3;
 import com.capdevon.input.GInputAppState;
 import com.capdevon.physx.Physics;
 import com.capdevon.physx.PhysxDebugAppState;
@@ -19,7 +12,7 @@ import com.jme3.system.AppSettings;
 import mygame.player.PlayerManager;
 
 /**
- *
+ * @author capdevon
  */
 public class Main extends SimpleApplication {
 
@@ -34,7 +27,7 @@ public class Main extends SimpleApplication {
         AppSettings settings = new AppSettings(true);
         settings.setTitle("Archer-Game-Template-2");
         settings.setUseJoysticks(true);
-        settings.setResolution(1280, 720);
+        //settings.setResolution(1280, 720);
         //settings.setSamples(4);
         //settings.setBitsPerPixel(32);
         //settings.setFrameRate(60);
@@ -51,7 +44,6 @@ public class Main extends SimpleApplication {
         stateManager.detach(stateManager.getState(FlyCamAppState.class));
         flyCam.setEnabled(false);
 
-        JMonkey3.initEngine(this);
         SoundManager.init(assetManager);
 
         /** Initialize the physics simulation */
