8997d18b6f21d715aba06c41b8e5ea4b841bb65e
capdevon <jefferydeaver2010@gmail.com>
2022-11-13 14:20:16 +0100

    added monsters with simple AI

--- src/main/java/mygame/Main.java
+++ src/main/java/mygame/Main.java
@@ -51,7 +51,7 @@ public class Main extends SimpleApplication {
         
         // preload models
         assetManager.loadModel(AnimDefs.Archer.ASSET_PATH);
-        //assetManager.loadModel(AnimDefs.Monster.ASSET_PATH);
+        assetManager.loadModel(AnimDefs.Monster.ASSET_PATH);
 
         /** Initialize the physics simulation */
         BulletAppState physics = new BulletAppState();
@@ -62,7 +62,7 @@ public class Main extends SimpleApplication {
         stateManager.attach(new GInputAppState());
         stateManager.attach(new PlayerManager());
         stateManager.attach(new CubeAppState());
-        //stateManager.attach(new MonsterAppState());
+        stateManager.attach(new MonsterAppState());
         stateManager.attach(new TogglePhysicsDebugState());
 
         //String dirName = System.getProperty("user.dir") + "/video";
