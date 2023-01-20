c87fd220924c1f71e13b64b83cf6748a5006d587
Stephen Gold <sgold@sonic.net>
2023-01-19 18:10:57 -0800

    Damageable:  refactor to use MySpatial instead of an ad-hoc method

--- game/src/main/java/mygame/controls/Damageable.java
+++ game/src/main/java/mygame/controls/Damageable.java
@@ -9,6 +9,7 @@ import com.jme3.material.Material;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.SceneGraphVisitorAdapter;
 import com.jme3.scene.Spatial;
+import jme3utilities.MySpatial;
 
 /**
  * 
@@ -24,7 +25,7 @@ public class Damageable extends AdapterControl {
             return;
 
         hasAppliedDamage = true;
-        List<Geometry> lstGeometries = listGeometries(spatial);
+        List<Geometry> lstGeometries = MySpatial.listGeometries(spatial);
 
         for (Geometry geo: lstGeometries) {
             Material mat = geo.getMaterial();
@@ -44,18 +45,6 @@ public class Damageable extends AdapterControl {
                 System.out.println(spatial + "; MaterializeTimer added");
             }
         }
-    }
-
-    private List<Geometry> listGeometries(Spatial subtree) {
-        List<Geometry> storeResult = new ArrayList<>();
-        subtree.breadthFirstTraversal(new SceneGraphVisitorAdapter() {
-            @Override
-            public void visit(Geometry geo) {
-                storeResult.add(geo);
-            }
-        });
-
-        return storeResult;
     }
 
     private void destroy() {
