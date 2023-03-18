e4078820580710ee6adc1dbb23c2b58e86864549
Stephen Gold <sgold@sonic.net>
2023-03-18 13:44:14 -0700

    PlayerControl:  damp the overall rotation of the avatar

--- game/src/main/java/mygame/player/PlayerControl.java
+++ game/src/main/java/mygame/player/PlayerControl.java
@@ -8,10 +8,12 @@ import com.capdevon.anim.Animator;
 import com.capdevon.control.AdapterControl;
 import com.jme3.audio.AudioNode;
 import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.scene.Spatial;
+import jme3utilities.math.MyMath;
 import jme3utilities.math.MyVector3f;
 
 import mygame.AnimDefs.Archer;
@@ -44,6 +46,10 @@ public class PlayerControl extends AdapterControl {
     public float runSpeed = 5.5f;
     public float moveSpeed = 4.5f;
     public float turnSpeed = 10f;
+    /**
+     * control the overall rotation of the avatar via BetterCharacterControl
+     */
+    private final Damper bccTurner = new Damper(0.4f, -0.2f);
 
     @Override
     public void setSpatial(Spatial sp) {
@@ -64,11 +70,19 @@ public class PlayerControl extends AdapterControl {
         camDir.setY(0);
         camDir.normalizeLocal();
 
+        bcc.getViewDirection(viewDirection);
+        float viewAzimuth = MyVector3f.azimuth(viewDirection);
+
         if (m_PlayerWeaponManager.isAiming) {
             // Never walk while you're aiming.
             bcc.setWalkDirection(Vector3f.ZERO);
-            bcc.setViewDirection(camDir);
             footstepsSFX.stop();
+            float cameraAzimuth = MyVector3f.azimuth(camDir);
+            float azimuthErr = cameraAzimuth - viewAzimuth;
+            {
+                azimuthErr = MyMath.standardizeAngle(azimuthErr);
+                bccTurner.setNextError(azimuthErr);
+            }
 
         } else { // not aiming a weapon
             camera.getLeft(camLeft);
@@ -91,7 +105,10 @@ public class PlayerControl extends AdapterControl {
 
             if (!MyVector3f.isZero(walkDirection)) {
                 walkDirection.normalizeLocal();
-                bcc.setViewDirection(walkDirection);
+                float walkAzimuth = MyVector3f.azimuth(walkDirection);
+                float azimuthErr = walkAzimuth - viewAzimuth;
+                azimuthErr = MyMath.standardizeAngle(azimuthErr);
+                bccTurner.setNextError(azimuthErr);
             }
 
             float xSpeed = isRunning ? runSpeed : moveSpeed;
@@ -112,6 +129,13 @@ public class PlayerControl extends AdapterControl {
                 footstepsSFX.stop();
             }
         }
+
+        // Update the avatar's view direction.
+        float controlSignal = bccTurner.update();
+        float x = FastMath.cos(controlSignal);
+        float z = FastMath.sin(controlSignal);
+        viewDirection.set(x, 0, z);
+        bcc.setViewDirection(viewDirection);
     }
 
     private void setAnimTrigger(Animation3 newAnim) {
